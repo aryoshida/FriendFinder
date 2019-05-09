bda9f6f8adbcae75d7a05f5125fc7876e961c9de
Andrea Yoshida <aryoshida9340@gmail.com>
2019-05-09 17:06:53 -0600

    updated the js

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+var routes = require("")
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 app.listen(PORT, function(){
     console.log("server listening on port: " + PORT);
